065236bc075ee210a0bc0a472e6eb06b8968142f
Micky Fazler <57700946+mickyfazler@users.noreply.github.com>
2022-12-02 15:13:33 +0600

    Update mainbb.js

--- jsbb/mainbb.js
+++ jsbb/mainbb.js
@@ -577,11 +577,16 @@ let iceServersh = {     // "h" in last means html
     iceServers: [
     //   {
     //     "urls": [
-    //         "stun:stun.counterpath.com"
+
     //         // "stun:stun.l.google.com:19302", 
     //         // "stun:stun1.l.google.com:19302", 
     //         // "stun:stun2.l.google.com:19302"
     // ]
+      {
+        "urls": [
+            "stun:openrelay.metered.ca:80"
+    ]
+},
     {
         urls: "turn:openrelay.metered.ca:443",
         username: "openrelayproject",
